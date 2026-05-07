0cd458b55d4dd72ebaf32498db6581230784b90d
Niloofar-didar <niloofar.didar@ymail.com>
2026-05-07 10:43:59 -0700

    chatBot that answers user's input question through connection to openAPI, while saving the history of conversation

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Requirements to install:
+run below lines on your terminal to install Langchain lib and work with openAPI:
+1-pip install openai langchain-openai langchain-core python-dotenv
+2-core LLM pipeline—prompting, context injection, and response parsing.
+
+Projects I start with in order:
+
+1- chatBot.py
+This is a simple chatBot that connects to OpenAPI directly and answers user's input question while saving the history of conversation.
+The conversation sample are published into chat_with_bot.txt. I've limited the max_token to 30, that's why some answers may be incomplete! 
---
+++ chatBot.py
@@ -0,0 +1,72 @@
+#
+'''
+This is a sample proejct of basic connection to OPENAI API directly
+message is a dic with role and content as user's msg
+'''
+import json
+
+from openai import OpenAI
+from dotenv import load_dotenv
+from datetime import datetime
+
+
+#api key loading
+load_dotenv()
+client= OpenAI()
+selected_model= "gpt-4o-mini"
+maxToken=30
+
+
+userQuery= "what's the most rewarding engineering major in 2026? answer shortly in one sentence"
+
+def simpleQA ():
+    response= client.chat.completions.create(
+    model=selected_model,
+    messages= [
+        {
+        "role":"user",
+        "content": userQuery
+        }
+    ], max_tokens=maxToken)
+    print(response.choices[0].message.content)
+
+#calls openAPI and stores the result as assistance response into the history of chats
+
+def askBot(messagesHist):
+
+    response= client.chat.completions.create(
+        messages=messagesHist, model=selected_model, max_tokens=maxToken)
+    responseContent=response.choices[0].message.content
+    # update the history
+    addMsgToHistory(messagesHist, responseContent, "assistant")
+    return responseContent
+
+# helper function to update the history
+def addMsgToHistory(messagesHist, content, role):
+    messagesHist.append({"role": role, "content": content})
+
+
+############################# Starting the main chatBot app #################
+ # add a system instruction for the whole conversation from AI response to be brief
+messagesHist=[{"role": "system", "content": "Be concise. Answer in one or two sentences only."}]
+
+print("Starting chat with a bot. If you want to finish, type 'exit'")
+userQuery=input("> ")
+while(userQuery!="exit"):
+
+    addMsgToHistory(messagesHist,userQuery ,"user")
+    result_1 = askBot(messagesHist)
+    print(result_1)
+    userQuery = input("> ")
+
+#writing to a txt file
+with open ("chat_with_bot.txt", 'a') as file:
+    currentT= datetime.now().strftime("%Y-%m-%d %H:%M")
+    file.write(f"new chat\n Time: [{currentT}]\n")
+    for entry in messagesHist:
+        file.write(str(entry))
+        file.write("\n")
+
+    file.write( "\n")
+
+print("Conversation ended! Enjoy your day!")
